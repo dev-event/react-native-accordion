5a4d649aeedfec134a80b9b694a8c4b3bf3f8414
David Axelrod <daxaxelrod@gmail.com>
2022-05-12 12:55:16 -0400

    upgrade

--- lib/typescript/hooks/index.d.ts
+++ lib/typescript/hooks/index.d.ts
@@ -1,2 +1,1 @@
-export { default as useLayout } from './use-layout';
 export { default as useValidator } from './use-validator';
--- lib/typescript/hooks/use-layout/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutChangeEvent } from 'react-native';
-declare const useLayout: (defaultHeight?: number) => readonly [{
-    height: number;
-    width: number;
-    measured: boolean;
-}, (e: LayoutChangeEvent) => void];
-export default useLayout;
